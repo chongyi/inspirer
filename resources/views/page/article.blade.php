9384d9c53d270963a5b2397be0632acdf226ec21
chongyi <xpz3847878@163.com>
2015-04-02 18:12:58 +0800

    多说评论插件

--- resources/views/page/article.blade.php
+++ resources/views/page/article.blade.php
@@ -25,6 +25,30 @@
             {!! \App\Inspirer\ArticleProcess::getContent($article->content) !!}
         </div>
     </article>
+    <!-- 多说评论框 start -->
+    <div class="ds-thread" data-thread-key="{{ $article->id }}" data-title="{{ $article->title  }}" data-url="{{ url('article', [$article->id])  }}"></div>
+    <!-- 多说评论框 end -->
+    <!-- 多说公共JS代码 start (一个网页只需插入一次) -->
+    <script type="text/javascript">
+    var duoshuoQuery = {short_name:"insp"};
+    	(function() {
+    		var ds = document.createElement('script');
+    		ds.type = 'text/javascript';ds.async = true;
+    		ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+    		ds.charset = 'UTF-8';
+    		(document.getElementsByTagName('head')[0]
+    		 || document.getElementsByTagName('body')[0]).appendChild(ds);
+    	})();
+    	</script>
+    <!-- 多说公共JS代码 end -->
+    <style>
+    #ds-thread { margin-left: 240px; border-left: solid 1px #2180C5; padding: 15px; border-top: dotted 1px #2180c5; }
+    .ds-textarea-wrapper { background:transparent!important; border: solid 1px #2180c5!important; }
+    .ds-textarea-wrapper textarea { color: #2180c5!important; }
+    .ds-post-toolbar { box-shadow: none!important; }
+    .ds-post-options { border-top: 0!important; background: transparent!important; border-color: #2180c5!important; box-sizing: border-box!important; }
+    .ds-post-button { border: 0!important; background: #2180c5!important; color: #fcfcfc!important; text-shadow: none!important; box-shadow: none!important; box-sizing: border-box!important; height: 30px!important; }
+    </style>
 </div>
 <div class="widget-container">
     <div class="widget category-view-widget">
