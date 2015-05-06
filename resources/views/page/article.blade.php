7f83619d5ba592a42c17fd6bbbd4b9a37ed2d289
chongyi <xpz3847878@163.com>
2015-05-06 11:14:30 +0800

    fixed

--- resources/views/page/article.blade.php
+++ resources/views/page/article.blade.php
@@ -20,12 +20,12 @@
                 <ul class="c-post-meta">
                     <li><i class="fa fa-calendar"></i>{{ date('Y-m-d', strtotime($article->created_at)) }}</li>
                     <li><i class="fa fa-clock-o"></i>{{ date('H:i', strtotime($article->created_at)) }}</li>
-                    <li><i class="fa fa-inbox"></i><a title="{{ $article->category->display_name }}" href="{{ url('category', $article->category_id) }}">{{ $article->category->display_name }}</a></li>
+                    <li><i class="fa fa-inbox"></i><a title="{{ $article->category->display_name }}" href="@if(!empty($article->category->name)){{ url('category', $article->category->name) }}@else{{ url('category', $article->category->id) }}@endif">{{ $article->category->display_name }}</a></li>
                     @if($article->sort > 0)<li><i class="fa fa-arrow-up"></i>TOP</li>@endif
                     @if(count($article->tags))
                     <li><i class="fa fa-tags"></i>
                         @foreach($article->tags as $tag)
-                        <span class="label label-default"><a href="{{ url('tag', $tag->id) }}">{{ $tag->display_name }}</a></span>
+                        <span class="label label-default"><a href="@if(!empty($tag->name)){{ url('tag', $tag->name) }}@else{{ url('tag', $tag->id) }}@endif">{{ $tag->display_name }}</a></span>
                         @endforeach
                     </li>
                     @endif
