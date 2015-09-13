4195af54fb85454194716e90e76650e8c2908462
chongyi <xpz3847878@163.com>
2015-09-13 11:45:36 +0800

    修正小 BUG

--- app/Providers/InspirerProvider.php
+++ app/Providers/InspirerProvider.php
@@ -5,32 +5,33 @@ use Illuminate\Pagination\Paginator;
 use Illuminate\Pagination\AbstractPaginator;
 use Illuminate\Support\ServiceProvider;
 
-class InspirerProvider extends ServiceProvider {
+class InspirerProvider extends ServiceProvider
+{
 
-	/**
-	 * Bootstrap the application services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
         // 创建 Xunsearch XS_APP_ROOT 常量
         define('XS_APP_ROOT', app_path('Inspirer/Xunsearch'));
 
         // 使用自定义分页模板
-		Paginator::presenter(function (AbstractPaginator $paginator) {
+        Paginator::presenter(function (AbstractPaginator $paginator) {
             return new PaginationPresenter($paginator);
         });
-	}
+    }
 
-	/**
-	 * Register the application services.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
 }
